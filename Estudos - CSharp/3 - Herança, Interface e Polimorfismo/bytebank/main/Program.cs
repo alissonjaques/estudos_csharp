5d95dddb3fb57541db1db4c8be4c2ad8d354fcb4
Alisson Jaques - Computer Engineer <alissonjaquesrmq@gmail.com>
2022-08-23 20:39:48 -0300

    Organização dos pacotes no modelo MVC e criação da classe Funcinario

---
+++ Estudos - CSharp/3 - Herança, Interface e Polimorfismo/bytebank/main/Program.cs
@@ -0,0 +1,17 @@
+﻿using bytebank.model.clientes;
+using bytebank.model.contas;
+using bytebank.model.funcionarios;
+
+Console.WriteLine("Bem vindo ao ByteBank!\n");
+Cliente titular = new Cliente("Alisson Jaques","111.111.111-11","Engenheiro da Computação");
+
+ContaCorrente conta = new ContaCorrente(titular, "4543136", (int)124, "Sicoob Minas", (double)30.0);
+conta.Depositar(10000);
+conta.Depositar(-1);
+conta.Sacar(500.55);
+Console.WriteLine("\n"+conta.ToString());
+ContaCorrente contaDefault = new ContaCorrente();
+conta.Transferir(500.55, contaDefault);
+Console.WriteLine("\n"+contaDefault.ToString());
+
+Console.ReadKey();
---
+++ Estudos - CSharp/3 - Herança, Interface e Polimorfismo/bytebank/model/funcionarios/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace bytebank.model.funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string nome, string cpf, double salario)
+        {
+            this.Nome = nome;
+            this.Cpf = cpf;
+            this.Salario = salario;
+        }
+
+        public Funcionario()
+        {
+            this.Nome = "";
+            this.Cpf = "";
+            this.Salario = 0.0;
+        }
+
+        public override string? ToString()
+        {
+            StringBuilder resultado = new StringBuilder();
+            resultado.Append("\nNome: ").Append(Nome);
+            resultado.Append("\nCPF: ").Append(Cpf);
+            resultado.Append("\nSalário: R$ ").Append(Salario);
+            return resultado.ToString();
+        }
+
+    }
+    
+}
+
+
